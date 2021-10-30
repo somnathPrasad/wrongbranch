2dd24784193dd5170c980fd41f731618378cd5e5
Somnath Prasad <somnathprasad559@gmail.com>
2021-10-30 11:43:25 +0530

    Initial commit

---
+++ components/card.js
@@ -0,0 +1,8 @@
+export default function Card(){
+    return(
+        <div className="bg-green-400 rounded-md max-w-lg px-5 py-2 mx-5 my-5 hover:bg-green-500 hover:shadow-2xl">
+            <h1 className="text-lg font-semibold">WAP to Print positive numbers</h1>
+            <p className="overflow-ellipsis">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores delectus tempora tenetur. Repellendus vitae quam quia, provident ipsa veniam incidunt similique exercitationem esse nam explicabo repellat ea, totam, possimus doloremque.</p>
+        </div>
+    )
+}
